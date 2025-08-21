640c95e5b7438db866cb12a812f8bf800a3fae85
KATONTICORP <katonticorp@gmail.com>
2025-08-21 02:13:50 -0500

    Create projects.js

---
+++ src/data/projects.js
@@ -0,0 +1,31 @@
+const projects = [
+  {
+    slug: "oasis-cancun",
+    name: "Oasis Residences Cancun",
+    location: "Cancun, Mexico",
+    description: "Luxury beachfront residences with world-class amenities and panoramic ocean views.",
+    investmentHighlights: {
+      yearlyPlusvalia: "12%",
+      vacationOccupancy: "82%",
+      avgNightCost: "$320 USD"
+    },
+    units: [
+      {
+        number: "101",
+        size: "120 m²",
+        price: "$320,000 USD",
+        floorPlan: "/images/floorplans/unit101.png",
+        available: true
+      },
+      {
+        number: "102",
+        size: "150 m²",
+        price: "$390,000 USD",
+        floorPlan: "/images/floorplans/unit102.png",
+        available: false
+      }
+    ]
+  }
+];
+
+export default projects;
